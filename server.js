5350b665eb817a697045afd4779433b509f68df7
chandra8120 <chandrasekhar8120@gmail.com>
2026-06-17 11:19:17 +0530

    changes

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 import "dotenv/config";
-import app  from  "./app.js"
-import  "./src/config/db.js"
+import app from "./app.js";
+import "./src/config/db.js";
 import { startCronJobs } from "./src/module/admin/sportmonks/sportmonks.cron.js";
+import { verifyMailer } from "./src/utils/mailer.js";
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
-  startCronJobs();  
-});  
-                   
+  await verifyMailer();
+  startCronJobs();
+});
